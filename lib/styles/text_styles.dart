7b4f7fa97510cc8ef8ad35b8700d8c9bdb16e583
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-05-22 12:11:11 +0700

    feat: Ensure consistent font family for onboarding text styles

--- lib/styles/text_styles.dart
+++ lib/styles/text_styles.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 
 class AppTextStyles {
   static const TextStyle onboardingTitle = TextStyle(
+    fontFamily: 'Geist',
     fontSize: 46,
     fontWeight: FontWeight.bold,
     color: Colors.white,
   );
 
   static const TextStyle onboardingDescription = TextStyle(
+    fontFamily: 'Geist',
     fontSize: 18,
     fontWeight: FontWeight.w300,
     color: Colors.white70,
   );
 
   static const TextStyle onboardingButton = TextStyle(
+    fontFamily: 'Geist',
     fontSize: 18,
     color: Colors.white,
     fontWeight: FontWeight.w500,
