403deee7c73cfba7c7c3c4698a877dd384628618
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-06-04 13:36:19 +0700

    feat: Refactor HomeScreen to StatefulWidget; implement AnimatedSwitcher and BottomNavigationBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:finlog/styles/text_styles.dart';
+import 'package:finlog/screens/dashboard_screen.dart';
+import 'package:finlog/screens/home_content_screen.dart';
+import 'package:finlog/screens/scan_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  int _previousIndex = 0; // New
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    DashboardScreen(),
+    HomeContentScreen(),
+    ScanScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _previousIndex = _selectedIndex; // New
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +49,55 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text('Welcome to FinLog!'), // Placeholder content
+      body: AnimatedSwitcher( // New
+        duration: const Duration(milliseconds: 300), // New
+        transitionBuilder: (Widget child, Animation<double> animation) { // New
+          final offsetAnimation = Tween<Offset>( // New
+            begin: Offset((_selectedIndex > _previousIndex ? 1.0 : -1.0), 0.0), // New
+            end: Offset.zero, // New
+          ).animate(animation); // New
+          return SlideTransition( // New
+            position: offsetAnimation, // New
+            child: child, // New
+          ); // New
+        },
+        key: ValueKey<int>(_selectedIndex), // New
+        child: _widgetOptions.elementAt(_selectedIndex), // New: Add a unique key for AnimatedSwitcher
       ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min, // New
+        children: [ // New
+          Container( // New
+            color: const Color(0xFFE2E8F0), // Color of the line, matching app bar
+            height: 1.0, // Thickness of the line
+          ), // New
+          BottomNavigationBar( // New
+            backgroundColor: Colors.white, // White background
+            elevation: 0, // Remove shadow
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svgs/pie-chart.svg', width: 24, height: 24),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svgs/home.svg', width: 24, height: 24),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset('assets/svgs/scan.svg', width: 24, height: 24),
+                label: '',
+              ),
+            ],
+            currentIndex: _selectedIndex, // New
+            selectedItemColor: Colors.blue.shade700,
+            unselectedItemColor: Colors.grey,
+            onTap: _onItemTapped,
+            type: BottomNavigationBarType.fixed,
+            selectedLabelStyle: const TextStyle(fontSize: 0), // Hide label space
+            unselectedLabelStyle: const TextStyle(fontSize: 0), // Hide label space
+          ),
+        ], // New
+      ), // New
     );
   }
 }
